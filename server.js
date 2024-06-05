290a14e7fdb1bf92fa2d9207339441433da0118e
Naveen <naveen.ndr97@gmail.com>
2024-06-05 08:42:14 +0530

    oops notes

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("Learning OOPS.");
+//Object
+//Factories Function
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log("draw");
+        }
+    }
+}
+
+//Constructor
+
+//Constructor should always start with Capital Letter.
+function Circle(radius) {
+    //this represent the current object which is using.
+    this.radius = radius;
+    this.draw = function () {
+        console.log("draw");
+    }
+    console.log(this)
+}
+ 
+
+// new keyword create the new object. By putting new keyword only we can get access to the this keyword. this keyword represent the current object. Or else the this will show global object.
+const circle = new Circle(1);
